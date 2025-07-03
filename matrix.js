e0dbe969615a704aac0fa166af7ba1ca9856a717
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-03 13:50:40 +0100

    Add files via upload

--- matrix.js
+++ matrix.js
@@ -1,23 +1,22 @@
 
-const canvas = document.getElementById("matrix");
+const canvas = document.getElementById("matrixCanvas");
 const ctx = canvas.getContext("2d");
-
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-
-let letters = Array(256).join("1").split("");
-
-const draw = () => {
+const letters = "01";
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+const drops = Array.from({ length: columns }, () => 1);
+function drawMatrix() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#0F0";
-  ctx.font = "14pt monospace";
-  letters.forEach((y_pos, index) => {
-    const text = String.fromCharCode(3e4 + Math.random() * 33);
-    const x_pos = index * 15;
-    ctx.fillText(text, x_pos, y_pos);
-    letters[index] = y_pos > canvas.height + Math.random() * 1e4 ? 0 : y_pos + 15;
+  ctx.font = fontSize + "px monospace";
+  drops.forEach((y, i) => {
+    const text = letters[Math.floor(Math.random() * letters.length)];
+    const x = i * fontSize;
+    ctx.fillText(text, x, y * fontSize);
+    drops[i] = y * fontSize > canvas.height && Math.random() > 0.975 ? 0 : y + 1;
   });
-};
-
-setInterval(draw, 33);
+}
+setInterval(drawMatrix, 33);
