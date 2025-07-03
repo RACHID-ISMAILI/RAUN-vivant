cd1db88d35cde1a63e8d70ec4663ea0c2a231005
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-03 11:28:27 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,15 +1,18 @@
-export const firebaseConfig = {
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
   authDomain: "raun-network.firebaseapp.com",
   projectId: "raun-network",
-  storageBucket: "raun-network.appspot.com",
+  storageBucket: "raun-network.firebasestorage.app",
   messagingSenderId: "541416001018",
   appId: "1:541416001018:web:c1d8518ec9181631206843",
   measurementId: "G-90SBGYPPZD"
 };
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+export { auth };
