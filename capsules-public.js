e336f16d33ed10ee2e50a53bf248a7bcec47fc45
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-04 13:49:02 +0100

    Add files via upload

--- capsules-public.js
+++ capsules-public.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   const capsulesContainer = document.getElementById("capsules");
 
@@ -7,26 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
         capsulesContainer.innerHTML = "";
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          const titre = data.title || "...";
-          const contenu = data.text?.trim()
-                          || data.content?.trim()
-                          || data.rappel?.trim()
-                          || data.alignement?.trim()
-                          || data.projection?.trim()
-                          || "Contenu vide";
-          const count = data.readCount || 0;
+          const contenu = data.text?.trim() || "Contenu vide";
 
           const capsule = document.createElement("div");
           capsule.innerHTML = `
             <hr />
-            <h3>${titre}</h3>
             <p><span style="color:lime">${contenu}</span></p>
-            <p>👁️ <strong>Lectures</strong> : ${count}</p>
-            <p>💬 <strong>Commentaires</strong> :</p>
-            <input placeholder="Votre nom" class="nom" />
-            <input placeholder="Votre commentaire" class="commentaire" />
-            <button onclick="envoyerCommentaire('${doc.id}', this)">Envoyer</button>
-            <div id="commentaires-${doc.id}"></div>
           `;
           capsulesContainer.appendChild(capsule);
         });
