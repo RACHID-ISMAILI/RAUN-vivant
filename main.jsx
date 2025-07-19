722c23cc975636307e7c349631c2bea17715196b
Rachid Ismaili <rachid.ismaili@gmail.com>
2025-07-19 14:18:38 +0100

    Projet propre RAUN-VIVANT-CLEAN

---
+++ main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <p style={{textAlign:'center'}}>Bienvenue dans RAUN-VIVANT 🌱</p>
+  </React.StrictMode>
+);
